5f122fa05d730eda02a041922322f10561cbd3e5
Solas <solas@kora-messenger.app>
2026-08-18 18:57:59 +0000

    Fix: remove unnecessary material import in crash_logger

--- lib/services/crash_logger.dart
+++ lib/services/crash_logger.dart
@@ -3,7 +3,6 @@ import 'dart:isolate';
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// Captures and persists every uncaught error in Kora Messenger.
